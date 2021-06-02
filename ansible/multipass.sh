f88d51c27e5f895b2e02e832fe563de4b0c4aedc
Drábóczi Balázs <draboczi.balazs@gmail.com>
2021-06-02 16:28:05 +0200

    playbooks

--- ansible/multipass.sh
+++ ansible/multipass.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-INSTANCE_NAME=test
+INSTANCE_NAME=multi21
+USER_NAME=$(cat cloud-config.yaml | grep name | awk '{print $3}')
 multipass.exe launch --name $INSTANCE_NAME -m 4G -d 20G -c 2 --cloud-init cloud-config.yaml
 INSTANCE_IP=$(multipass.exe info $INSTANCE_NAME | grep 'IPv4' | awk '{print $2}')
 
 INSTANCE_IP=${INSTANCE_IP//$'\r'/}
 echo -e "[multipass]" >hosts
-echo -e "$INSTANCE_IP    ansible_connection=ssh    ansible_user=ansible" >>hosts
+echo -e "$INSTANCE_IP    ansible_connection=ssh    ansible_user=$USER_NAME" >>hosts
 
-ansible-playbook mk8s.yaml -i hosts
+#ansible-playbook mk8s.yaml -i hosts
 
-ansible-playbook deployment.yaml -i hosts
+#ansible-playbook deployment.yaml -i hosts
 
-ansible-playbook helm.yaml -i hosts
+#ansible-playbook helm.yaml -i hosts
